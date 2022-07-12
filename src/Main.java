8894313a1333fc43d66a942d5f430af0c30caef4
Hotey777 <volksuper1@gmail.com>
2022-07-12 13:43:33 +0300

    Zadanie3.2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double y = 1.7; //Рост в местрах
-        double x = 56; //Вес в кг
-        double MyIndex = service.calculate(x,y);
+        double height = 1.7; //Рост в местрах
+        double weight = 56; //Вес в кг
+        double MyIndex = service.calculate(weight,height);
         System.out.println("Индекс массы тела:");
         System.out.println(MyIndex);
     }
